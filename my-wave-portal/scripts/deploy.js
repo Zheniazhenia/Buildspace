1fd51c0ec724e0d2cfb321b14756a5644d60d17b
zheniazhenia <cptyevgenii@ukr.net>
2022-11-04 16:31:00 +0200

    section3

--- my-wave-portal/scripts/deploy.js
+++ my-wave-portal/scripts/deploy.js
@@ -4,12 +4,14 @@ const main = async () => {
   
     console.log("Deploying contracts with account: ", deployer.address);
     console.log("Account balance: ", accountBalance.toString());
+    
   
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
-  
+
     console.log("WavePortal address: ", waveContract.address);
+
   };
   
   const runMain = async () => {
